21b4a61216904081eace25f8e2b2046b97c23391
unknown <neno.smg@hotmail.com>
2018-02-22 18:02:42 -0400

    Autoload do hcode BR tudo certo

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3a7504a37e07b0cea357d0160397285b
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit3a7504a37e07b0cea357d0160397285b
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit3a7504a37e07b0cea357d0160397285b::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3a7504a37e07b0cea357d0160397285b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3a7504a37e07b0cea357d0160397285b::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit3a7504a37e07b0cea357d0160397285b::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit3a7504a37e07b0cea357d0160397285b::$classMap;
 
